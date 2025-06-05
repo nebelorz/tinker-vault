58814a5a4fbf6d0dbeebefbf9468a575f1969a55
Alex Gassó <alex.gasso@col.vueling.com>
2025-06-05 16:50:37 +0200

    Refactor info + top contributors components

--- src/app/pages/home/top-contributors/top-contributors.component.ts
+++ src/app/pages/home/top-contributors/top-contributors.component.ts
@@ -1,31 +1,38 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { LucideAngularModule, AwardIcon } from "lucide-angular";
+import { LucideAngularModule, AwardIcon, Maximize2Icon, Minimize2Icon } from "lucide-angular";
 
 import { CONTRIBUTORS } from "../../../../assets/data/contributors";
 import type { Contributor } from "../../../interfaces/contributor.interface";
 
-import { ContributorsPopoverComponent } from "./more-contributors-popover/more-contributors-popover.component";
 import { MouseoverInfoComponent } from "./mouseover-info/mouseover-info.component";
 
 @Component({
     selector: "app-top-contributors",
     standalone: true,
-    imports: [CommonModule, LucideAngularModule, ContributorsPopoverComponent, MouseoverInfoComponent],
+    imports: [CommonModule, LucideAngularModule, MouseoverInfoComponent],
     templateUrl: "./top-contributors.component.html",
     styleUrls: ["./top-contributors.component.css"],
 })
 export class TopContributorsComponent {
     contributors: Contributor[] = CONTRIBUTORS;
-
     readonly iconAward = AwardIcon;
+    readonly iconMaximize = Maximize2Icon;
+    readonly iconMinimize = Minimize2Icon;
+
+    expanded = false;
 
     getTopContributors(amountOfContributors: number = 3): Contributor[] {
         return [...this.contributors].sort((a, b) => b.amount - a.amount).slice(0, amountOfContributors);
     }
 
-    getRestOfContributors(): Contributor[] {
-        return [...this.contributors].sort((a, b) => b.amount - a.amount).slice(3);
+    toggleContributors(event: Event) {
+        event.preventDefault();
+        this.expanded = !this.expanded;
+    }
+
+    get contributorsSorted(): Contributor[] {
+        return [...this.contributors].sort((a, b) => b.amount - a.amount);
     }
 }
--- src/assets/data/contributors.ts
+++ src/assets/data/contributors.ts
@@ -1,7 +1,7 @@
 export const CONTRIBUTORS = [
-  { name: 'user1', amount: 100 },
-  { name: 'user2', amount: 99 },
-  { name: 'user3', amount: 99 },
-  { name: 'user12', amount: 5 },
-  { name: 'user13', amount: 2 },
+    { name: "user1", amount: 100 },
+    { name: "user2", amount: 99 },
+    { name: "user3", amount: 99 },
+    { name: "user12", amount: 5 },
+    { name: "user13", amount: 2 },
 ];
